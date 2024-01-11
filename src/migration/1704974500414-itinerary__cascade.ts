542684be9b7a382f578c0c08d9c7a0b945f9506d
zahid-soft <zahidsarang95@gmail.com>
2024-01-11 17:36:03 +0530

    add migration for itinerary cascade

---
+++ src/migration/1704974500414-itinerary__cascade.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Itinerary_cascade1704974500414 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "itinerary" DROP CONSTRAINT "FK_fe8aa2e64e4676c7dd623eb6349"`,
+        );
+
+        await queryRunner.query(
+            `ALTER TABLE "itinerary" ADD CONSTRAINT "FK_fe8aa2e64e4676c7dd623eb6349" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "itinerary" DROP CONSTRAINT "FK_fe8aa2e64e4676c7dd623eb6349"`,
+        );
+
+        await queryRunner.query(
+            `ALTER TABLE "itinerary" ADD CONSTRAINT "FK_fe8aa2e64e4676c7dd623eb6349" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
+    }
+}
